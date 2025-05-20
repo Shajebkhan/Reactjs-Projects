f997d49ab57993f7395b840a6c264f2bb52c0fc9
shajebkhan <shajebkhanbisalpur@gmail.com>
2025-05-20 17:21:24 +0530

    some changes

--- Application_Form_CRUD/src/App.jsx
+++ Application_Form_CRUD/src/App.jsx
@@ -21,19 +21,22 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(values)
+    alert("Form submit")
+    resetFunc("Pass arguments by props after submit and reset");
   }
 
-  const resetFunc = () => {
-    setValues({ firstname: '', lastname: '', email: '' })
-  }
 
+  const resetFunc = (abc) => {
+    setValues({ firstname: '', lastname: '', email: '', contact: '', subject: '', resume: '', url: '', about: abc });
+  }
+  console.log(values)
   return (
     <>
       <div className='container'>
         <h1>Form in React</h1>
         <form onSubmit={handleSubmit}>
           <label htmlFor="firstname" >First Name</label>
-          <input type="text" placeholder='Enter First Name' name='firstname' onChange={(e) => handleChange(e)} required value={values.firstname} />
+          <input type="text" placeholder='Enter First Name' name='firstname' onChange={handleChange} required value={values.firstname} />
 
           <label htmlFor="lastname">Last Name</label>
           <input type="text" placeholder='Enter Last Name' name='lastname' onChange={(e) => handleChange(e)} required value={values.lastname} />
@@ -63,7 +66,7 @@ function App() {
           <input type="text" name="url" placeholder='Enter image URL' onChange={(e) => handleChange(e)} />
 
           <label htmlFor="about">About</label>
-          <textarea name="about" id="about" cols="30" rows="10" placeholder='Enter Descrption' onChange={(e) => handleChange(e)}></textarea>
+          <textarea name="about" id="about" cols="30" rows="10" placeholder='Enter Descrption' value={values.about} onChange={(e) => handleChange(e)}></textarea>
 
           <button type='button' onClick={resetFunc}>Reset</button>
           <button type='submit'>Submit</button>
